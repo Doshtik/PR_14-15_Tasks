8b3d0f015ad6686ee1de9cf421838dcade6906fe
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2025-03-04 14:55:28 +0500

    page 640

--- app/src/main/java/com/hfad/tasks/TaskDao.kt
+++ app/src/main/java/com/hfad/tasks/TaskDao.kt
@@ -10,11 +10,11 @@ import androidx.room.Update
 @Dao
 interface TaskDao {
     @Insert
-    fun insert(task: Task)
+    suspend fun insert(task: Task)
     @Update
-    fun update(task: Task)
+    suspend fun update(task: Task)
     @Delete
-    fun delete(task: Task)
+    suspend fun delete(task: Task)
     @Query("SELECT * FROM task_table WHERE taskId = :taskId")
     fun get(taskId: Long): LiveData<Task>
     @Query("SELECT * FROM task_table ORDER BY taskId DESC")
--- app/src/main/java/com/hfad/tasks/TasksFragment.kt
+++ app/src/main/java/com/hfad/tasks/TasksFragment.kt
@@ -14,6 +14,14 @@ class TasksFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentTasksBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        val application = requireNotNull(this.activity).application
+        val dao = TaskDatabase.getInstance(application).taskDao
+        val viewModelFactory = TasksViewModelFactory(dao)
+        val viewModel = ViewModelProvider(
+            this, viewModelFactory).get(TasksViewModel::class.java)
+        binding.viewModel = viewModel
+
         return view
     }
     override fun onDestroyView() {
