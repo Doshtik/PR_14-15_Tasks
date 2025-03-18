d8defb68cf0583594ba0049c78c5b2535d1b60ae
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2025-03-18 08:45:18 +0500

    The End of PR_14 page 651

--- app/src/main/java/com/hfad/tasks/TasksViewModelFactory.kt
+++ app/src/main/java/com/hfad/tasks/TasksViewModelFactory.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class TasksViewModelFactory(private val dao: TaskDao) : ViewModelProvider.Factory {
-    fun <T: ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T: ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TasksViewModel::class.java)) {
             return TasksViewModel(dao) as T
         }
