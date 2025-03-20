90cd69727b017b5b3302744efbd2f2e0f37645cf
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2025-03-20 12:52:06 +0500

    PR_15 page 668

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.recyclerview)
     implementation (libs.androidx.lifecycle.viewmodel.ktx)
     implementation (libs.androidx.lifecycle.livedata.ktx)
     implementation (libs.androidx.room.runtime)
